aa0ec77a69853775013d0d2962ef2d0d1c59c1b2
EnesYildiz24 <deine.email@example.com>
2025-07-15 23:56:39 +0200

    stripe fix 2

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,11 +11,11 @@ import { chatRouter } from './routes/chat';
 import { genreRouter } from './routes/genre';
 import bewertungRouter from './routes/bewertung';
 import bestellungRouter from './routes/bestellung';
-import {spielRouter } from './routes/spiel';
-import {cartsRouter } from './routes/cart';
+import { spielRouter } from './routes/spiel';
+import { cartsRouter } from './routes/cart';
 import checkoutRoutes from './routes/checkout';
 import webhookRoutes from './routes/stripe-webhook'; // Import webhookRoutes
-import {offerRouter} from './routes/offer'; // Import offersRouter
+import { offerRouter } from './routes/offer'; // Import offersRouter
 import { connectRoutes } from './routes/connect';
 import userGamesRouter from './routes/userGames';
 import { chatPopupRouter } from './routes/chatpopup';
@@ -25,12 +25,8 @@ const app = express();
 configureCORS(app);
 app.use(cookieParser());
 
-app.use(
-    '/api/stripe/webhook',
-    express.raw({ type: 'application/json' }),  
-    webhookRoutes
-  );
-  
+app.use('/api/stripe/webhook', webhookRoutes);
+
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
@@ -46,15 +42,13 @@ app.use('/api/chat', chatRouter);
 app.use('/api/genre', genreRouter);
 app.use('/api/bewertung', bewertungRouter);
 app.use('/api/bestellung', bestellungRouter);
-app.use("/api/games", spielRouter); 
-app.use("/api/cart", cartsRouter); 
+app.use('/api/games', spielRouter);
+app.use('/api/cart', cartsRouter);
 app.use('/api/checkout', checkoutRoutes);
 app.use('/api/offers', offerRouter);
 app.use('/api/connect', connectRoutes);
 app.use('/api/user/games', userGamesRouter);
 app.use('/api/chatpopup', chatPopupRouter);
-app.use('/api/upload',uploadRouter);
-
-
+app.use('/api/upload', uploadRouter);
 
 export default app;
